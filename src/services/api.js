f775d2c25c6ea13b1b9119bdd701141e7af911a6
Jaures <jauresacs2004@gmail.com>
2025-06-23 12:00:35 +0200

    feat: poke of routage front -> Api

---
+++ src/services/api.js
@@ -0,0 +1,31 @@
+// services/api.js
+const API_BASE_URL = 'http://localhost:XXXX/api';
+
+class ApiService {
+  async get(endpoint) {
+    const response = await fetch(`${API_BASE_URL}${endpoint}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('token')}`
+      }
+    });
+    return response.json();
+  }
+
+  async post(endpoint, data) {
+    const response = await fetch(`${API_BASE_URL}${endpoint}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('token')}`
+      },
+      body: JSON.stringify(data)
+    });
+    return response.json();
+  }
+}
+
+export const apiService = new ApiService();
+
+  //TODO: à faire lorsqu'ils auront besoin de faire tel ou tel chose et lorsque l'api sera prête
