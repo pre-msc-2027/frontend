d5c8b2be23e3ebee2fcb505e52dc9cbcc540b8b8
Watywombat <arthur.bartczak@epitech.eu>
2025-06-23 11:30:40 +0200

    Commit blob

---
+++ src/Component/Blob.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './AnimatedBlob.css';
+
+const AnimatedBlob: React.FC = () => {
+    return (
+        <div className="blob-container">
+            <div className="blob" />
+        </div>
+    );
+};
+
+export default AnimatedBlob;
