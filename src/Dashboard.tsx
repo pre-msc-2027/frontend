91afcd4a65c307722b6998426fa2894386a2ecef
Alexandre <alexandre.benoist@epitech.eu>
2025-06-30 10:11:46 +0200

    Dashboard.tsx add new component

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,18 +1,24 @@
 import "./Dashboard.css";
 import MenuRepo from "./Component/MenuRepo.tsx";
 import React from "react";
+import PieChart from "./Component/PieChart.tsx";
+import LogsDashboard from "./Component/LogsDashboard.tsx";
 
 const Dashboard: React.FC = () => {
     return (
-        <div className="w-screen h-screen flex">
-            <div className="w-1/6 h-4/6 flex self-center m-4">
+        <div className="w-screen h-screen flex flex-col lg:flex-row bg-bg">
+            <div className="w-ful lg:w-1/6 h-4/6 flex self-center m-4">
                 <MenuRepo>
-
 
                 </MenuRepo>
             </div>
-            <div>
-
+            <div className="lg:w-4/6 flex p-4 justify-center flex-col lg:flex-row lg:items-stretch items-center">
+                <div className=" w-fit md:w-2xl md:2/4 lg:w-2/6 lg:h-2/6 m-4">
+                    <PieChart/>
+                </div>
+                <div className="w-full md:2/4 lg:w-4/6 lg:h-4/6 m-4">
+                    <LogsDashboard/>
+                </div>
             </div>
         </div>
     );
