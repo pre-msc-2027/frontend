4e43db6e5a808dc661031acafba2c07c234f628e
Watywombat <arthur.bartczak@epitech.eu>
2025-07-21 16:53:36 +0200

    add apiService.ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 // main.tsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // ✅ This is essential!
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.tsx';
 
+//import { apiService } from './apiService.ts';
+
+
+//apiService.setToken();
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
