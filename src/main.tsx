55501d7357ad39f9b7125072e62a82053a0dac61
benoista <alexandre.benoist@epitech.eu>
2025-09-03 14:21:49 +0200

    Fix(main) Fix SonarCube Analysis,

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.tsx';
 
-//import { apiService } from './apiService.ts';
-
-
-//apiService.setToken();
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
